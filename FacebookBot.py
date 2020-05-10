410e46fa56397b8b0104b6fa9d51ccbeb8ce5359
Free Malvare <64208836+Malvare17@users.noreply.github.com>
2020-05-10 20:20:02 +0200

    Update FacebookBot.py

--- FacebookBot.py
+++ FacebookBot.py
@@ -1,17 +1,16 @@
-from fbchat import log, Client,Message
+import fbchat as Facebook
 
-class EchoBot(Client):
+class EchoBot(Facebook.Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(thread_id, message_object.uid)
         self.markAsRead(thread_id)
        
-        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+        Facebook.log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
 
         reply = "I am currently unavailable, but I will contact you shortly. "
 
-        # If you're not the author, echo
         if author_id != self.uid:
-           self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
+           self.send(Facebook.Message(text=reply), thread_id=thread_id, thread_type=thread_type)
         self.markAsDelivered(author_id, thread_id)
 
 client = EchoBot("Login", "Password")
